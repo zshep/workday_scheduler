de13ffbd2a5f86755e8076fa883621528815dc38
zsheperdigian <zsheperdigian@gmail.com>
2022-08-02 16:03:25 -0700

    fixed the java script order. i now have todays date on page load up

---
+++ script.js
@@ -0,0 +1,17 @@
+// workday scheduler code
+
+//write function to display current day at top of callendar 
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+
+
+//add in code to change color of blocks as time passes(past, present, future)
+
+
+//add in click event into a block of time and add event 
+        //add ability to save event in local storage
+
+
+        
+
+

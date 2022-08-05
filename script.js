4f13375d548d206033c4bd9582c26443a1063742
zsheperdigian <zsheperdigian@gmail.com>
2022-08-05 15:40:52 -0700

    after working w/ tutor, added in four loop to iterate through array each hr

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 // workday scheduler code
-var time9 = document.querySelector("#am9");
-var time10 = document.querySelector("#am10");
-var time11 = document.querySelector("#am11");
-var time12 = document.querySelector("#pm12");
-var time01 = document.querySelector("#pm1");
-var time02 = document.querySelector("#pm2");
-var time03 = document.querySelector("#pm3");
-var time04 = document.querySelector("#pm4");
-var time05 = document.querySelector("#pm5");
+// variable to grab all the time blocks 
+var timeblock = document.querySelectorAll("hour");
+
 
 //write function to display current day at top of callendar 
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
+console.log(today.format("H"));
 //add in code to change color of blocks as time passes(past, present, future)
 var colorCode = function(){
 
-
+for (let index = 9; index < timeblock.length; index++) {
+        
+        
+}
 
 }
 
 
 //add in click event into a block of time and add event 
         //add ability to save event in local storage
+
+//  GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+        // use moment
+// WHEN I scroll down
+// THEN I am presented with time blocks for standard business hours
+        // hour, user input, save button
+// WHEN I view the time blocks for that day
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+        //  change classes
+// WHEN I click into a time block
+// THEN I can enter an event
+        // input || textArea
+// WHEN I click the save button for that time block
+        // save function
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
+        // get from local storage
+

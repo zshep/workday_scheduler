8aedbdcb84ffd2c61828052bef04342ebf886f14
zsheperdigian <zsheperdigian@gmail.com>
2022-08-08 13:06:23 -0700

    continued working on chaning the color of each time row based on current time.0

--- script.js
+++ script.js
@@ -1,37 +1,68 @@
 // workday scheduler code
-// variable to grab all the time blocks 
-var timeblock = document.querySelectorAll("hour");
+// variable to grab all the rows (timeblocks)
+//let timeblock = document.querySelectorAll(".row");
+var today = moment();
+var curhour = today.format("H");
 
 
 //write function to display current day at top of callendar 
-var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-console.log(today.format("H"));
-//add in code to change color of blocks as time passes(past, present, future)
+
+//function to change color of blocks as time passes(past, present, future)
 var colorCode = function(){
-
-for (let index = 9; index < timeblock.length; index++) {
-        
+let timeblock = document.querySelectorAll(".row");
+//debugging
+console.log(timeblock); //
+console.log(curhour); // current hour
+console.log("the function is running");
+//run through the array and set past, present, future
+for (let i = 0; i < timeblock.length; i++) {
+        let curTime = timeblock[i];
+        let viewT = curTime.dataset.num;
+        console.log(viewT);
+        //if statement to compare current hour w/ each color div
+        if (viewT < curhour) {
+                //set timeblock to have class of past only
+                console.log(viewT);
+                console.log("past");
+                $(this).addClass("past");
+                $(this).removeClass("future present");
+                               
+        }
+                if (viewT == curhour) {
+                // set timeblock to have class present only
+                console.log("present");
+                $(this).addClass("present");
+                $(this).removeClass("future past");
+                
+                 }
+                else {
+                // set timeblock to have class future only
+                console.log("future");
+                $(this).addClass("future");
+                $(this).removeClass("present past");
+                
+                }
         
 }
 
 }
+ colorCode();
 
 
-//add in click event into a block of time and add event 
+//creat var. to hold users click
+
+
+ //add in click event into a block of time and add event 
         //add ability to save event in local storage
 
-//  GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-        // use moment
+
+    
 // WHEN I scroll down
 // THEN I am presented with time blocks for standard business hours
         // hour, user input, save button
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-        //  change classes
+
 // WHEN I click into a time block
 // THEN I can enter an event
         // input || textArea

b7e866561ec5ca7d2e3dd4491de7c0b82c08fd74
zsheperdigian <zsheperdigian@gmail.com>
2022-08-08 21:02:55 -0700

    attempted to add in the savebutton

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Work Day Scheduler Starter Code
+
+The workDay scheduler shows the workday in a nice neat organized manner. The workscheduler will automatically update the workspace showing what times have already past, what times are yet to come and what the current hour is using color codes.
+
+The user can click and save an event based on the time when it is entered into.
+
+
+
--- script.js
+++ script.js
@@ -3,65 +3,60 @@
 //let timeblock = document.querySelectorAll(".row");
 var today = moment();
 var curhour = today.format("H");
-
+var saveBTN = $('.saveBtn');
 
 //write function to display current day at top of callendar 
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
+// brings up all saved items from localstorage
+localStorage.getItem("myEvent");
 
 //function to change color of blocks as time passes(past, present, future)
 var colorCode = function(){
 let timeblock = document.querySelectorAll(".row");
 //debugging
-console.log(timeblock); //
+console.log(timeblock);
 console.log(curhour); // current hour
 console.log("the function is running");
 //run through the array and set past, present, future
 for (let i = 0; i < timeblock.length; i++) {
-        let curTime = timeblock[i];
-        let viewT = curTime.dataset.num;
+        
+        let viewT = timeblock[i].dataset.num;
         console.log(viewT);
         //if statement to compare current hour w/ each color div
         if (viewT < curhour) {
                 //set timeblock to have class of past only
-                console.log(viewT);
+                console.log(`the current hour is ${curhour}`)
+                console.log(`The current timeblock is ${viewT}`);
                 console.log("past");
-                $(this).addClass("past");
-                $(this).removeClass("future present");
-                               
-        }
+                $(timeblock[i]).addClass("past");
+                $(timeblock[i]).removeClass("future present");
+
+                }
                 if (viewT == curhour) {
                 // set timeblock to have class present only
+                console.log(`the current hour is ${curhour}`)
+                console.log(`The current timeblock is ${viewT}`);
                 console.log("present");
-                $(this).addClass("present");
-                $(this).removeClass("future past");
+                $(timeblock[i]).addClass("present");
+                $(timeblock[i]).removeClass("future past");
                 
                  }
-                else {
+                if (viewT > curhour) {
                 // set timeblock to have class future only
+                console.log(`the current hour is ${curhour}`)
+                console.log(`The current timeblock is ${viewT}`);
                 console.log("future");
-                $(this).addClass("future");
-                $(this).removeClass("present past");
+                $(timeblock[i]).addClass("future");
+                $(timeblock[i]).removeClass("present past");
                 
                 }
         
-}
+}}
 
-}
- colorCode();
-
-
-//creat var. to hold users click
-
-
- //add in click event into a block of time and add event 
-        //add ability to save event in local storage
-
-
-    
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-        // hour, user input, save button
+// function to work save button
+{
+        
 
 // WHEN I click into a time block
 // THEN I can enter an event
@@ -72,4 +67,26 @@ for (let i = 0; i < timeblock.length; i++) {
 // WHEN I refresh the page
 // THEN the saved events persist
         // get from local storage
+
+}
+//action code
+ colorCode();
+
+$("button").on("click", function(event){
+        let userPick = event.target;
+        console.log(userPick);
+        localStorage.setItem("myEvent", $('textarea'))
+
+
+}
+        
+)
+
+
+
+ 
+
+
+
+
 
